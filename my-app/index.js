42f80364142b8e167da1b9ba568d3442467cb906
Pantea Khodabakhsh <pantea.khodabakhsh@student.ap.be>
2026-05-22 02:05:31 +0200

    update

--- my-app/index.js
+++ my-app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Pantea! 🚀");
+  res.send("Hello from Pantea!");
 });
 
 app.listen(3000, () => {
